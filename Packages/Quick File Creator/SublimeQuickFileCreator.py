0102dc7b719431900fb96653ef9a28f31b919909
Eugene Burmako <xeno.by@gmail.com>
2012-05-03 09:41:47 +0200

    multiple roots for quick file creator

--- Packages/Quick File Creator/SublimeQuickFileCreator.py
+++ Packages/Quick File Creator/SublimeQuickFileCreator.py
@@ -5,27 +5,35 @@ import sublime_plugin
 
 
 class QuickCreateFileCreatorBase(sublime_plugin.WindowCommand):
-    ROOT_DIR_PREFIX = '[root: '
-    ROOT_DIR_SUFFIX = ']'
-
     def doCommand(self):
-        if not self.find_root():
+        if not self.find_roots():
             return
 
         self.construct_excluded_pattern()
-        self.build_relative_paths()
+        self.init_relative_paths()
+        for root in self.roots:
+            self.build_relative_paths(root)
         self.move_current_directory_to_top()
         self.window.show_quick_panel(self.relative_paths, self.dir_selected)
 
-    def find_root(self):
+    def find_roots(self):
         folders = self.window.folders()
         if len(folders) == 0:
-            sublime.error_message('Could not find project root')
+            sublime.error_message('Could not find project roots')
             return False
 
-        self.root = folders[0]
-        self.rel_path_start = len(self.root) + 1
+        self.roots = folders
         return True
+
+    def find_dir_root(self, dir):
+        for folder in self.window.folders():
+            if dir.startswith(folder):
+                return folder
+            rel_path_start = folder.rindex("\\") + 1
+            alias = folder[rel_path_start:]
+            if dir.startswith(alias):
+                return folder
+        return ""
 
     def construct_excluded_pattern(self):
         patterns = [pat.replace('|', '\\') for pat in self.get_setting('excluded_dir_patterns')]
@@ -47,21 +55,30 @@ class QuickCreateFileCreatorBase(sublime_plugin.WindowCommand):
             results = settings.get(key)
         return results
 
-    def build_relative_paths(self):
-        self.relative_paths = [self.ROOT_DIR_PREFIX + os.path.split(self.root)[-1] + self.ROOT_DIR_SUFFIX]
-        for base, dirs, files in os.walk(self.root):
+    def init_relative_paths(self):
+        self.relative_paths = []
+
+    def build_relative_paths(self, root):
+        rel_path_start = root.rindex("\\") + 1
+        relative_path = root[rel_path_start:]
+        self.relative_paths.append(relative_path)
+
+        for base, dirs, files in os.walk(root):
             dirs_copy = dirs[:]
             [dirs.remove(dir) for dir in dirs_copy if self.excluded.search(dir)]
 
             for dir in dirs:
-                relative_path = os.path.join(base, dir)[self.rel_path_start:]
+                relative_path = os.path.join(base, dir)[rel_path_start:]
                 self.relative_paths.append(relative_path)
 
     def move_current_directory_to_top(self):
         view = self.window.active_view()
 
         if view:
-            cur_dir = os.path.dirname(view.file_name())[self.rel_path_start:]
+            file_dir = os.path.dirname(view.file_name())
+            file_root = self.find_dir_root(file_dir)
+            rel_path_start = file_root.rindex("\\") + 1
+            cur_dir = file_dir[rel_path_start:]
             for path in self.relative_paths:
                 if path == cur_dir:
                     i = self.relative_paths.index(path)
@@ -74,12 +91,11 @@ class QuickCreateFileCreatorBase(sublime_plugin.WindowCommand):
             self.window.show_input_panel(self.INPUT_PANEL_CAPTION, '', self.file_name_input, None, None)
 
     def file_name_input(self, file_name):
-        if self.selected_dir.startswith(self.ROOT_DIR_PREFIX):
-            dir = ''
-        else:
-            dir = self.selected_dir
+        dir = self.selected_dir
 
-        full_path = os.path.join(self.root, dir, file_name)
+        dir_root = self.find_dir_root(dir)
+        dir_root = dir_root[:dir_root.rindex("\\")]
+        full_path = os.path.join(dir_root, dir, file_name)
         if os.path.lexists(full_path):
             sublime.error_message('File already exists:\n%s' % full_path)
             return
