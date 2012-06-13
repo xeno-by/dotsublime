61922eb0d87a8b40e1f95d450f1842d466f214e7
Eugene Burmako <xeno.by@gmail.com>
2012-06-13 13:31:12 +0200

    fix relayout

---
+++ Packages/MyLayout/MyLayout.py
@@ -0,0 +1,61 @@
+import sublime, sublime_plugin
+import time
+
+class LayoutDaemon(sublime_plugin.EventListener):
+  def on_new(self, view):
+    # print "on_new: " + (view.name() or view.file_name() or "")
+    sublime.set_timeout(lambda: self.recheck_new(view), 100)
+
+  def recheck_new(self, view):
+    # print "recheck_new: " + (view.name() or view.file_name() or "")
+    if self.is_special(view):
+      if self.window and self.window.num_groups() > 1:
+        g, i = self.window.get_view_index(view)
+        if g != 1:
+          self.window.set_view_index(view, 1, len(self.window.views_in_group(1)))
+
+  def on_deactivated(self, view):
+    # print "on_deactivated: " + (view.name() or view.file_name() or "")
+    self.pv = view
+
+  def on_activated(self, view):
+    # print "on_activated: " + (view.name() or view.file_name() or "")
+    self.cv = view
+    self.window = view.window()
+    if self.needs_relayout():
+      print "needs relayout: " + (view.name() or view.file_name() or "")
+      self.relayout()
+
+  def is_special(self, view):
+    name = view.name() or ""
+    if name.startswith("myke ") or name == "Find Results":
+      return True
+
+  def needs_relayout(self):
+    if hasattr(self, "pv") and hasattr(self, "cv") and self.window and self.window.num_groups() > 1:
+      pg, pi = self.window.get_view_index(self.pv)
+      cg, ci = self.window.get_view_index(self.cv)
+      if pg == cg and self.pv != self.cv:
+        pn = self.pv.name() or ""
+        if pn.startswith("myke ") or pn == "Find Results":
+          # relayout only if we jump into a new view by the means of double-clicking
+          # if we navigate normally, there's no need in relayouting
+          delta_t = time.time() - self.last_selection_modified
+          delta_n = len(self.window.views()) - self.last_view_count
+          print delta_n
+          return delta_t < 0.2 and delta_n != 0
+
+  def on_selection_modified(self, view):
+    if view.window():
+      self.last_view_count = len(view.window().views())
+    self.last_selection_modified = time.time()
+
+  def relayout(self):
+    # crashes!! very weird
+    # pg, pi = self.window.get_view_index(self.pv)
+    # if pg != 1:
+    #   self.window.set_view_index(self.pv, 1, len(self.window.views_in_group(1)))
+    cg, ci = self.window.get_view_index(self.cv)
+    if cg != 0 and not self.is_special(self.cv):
+      self.window.set_view_index(self.cv, 0, len(self.window.views_in_group(0)))
+    self.window.focus_view(self.cv)
--- Packages/MyLayout/MySplit.py
+++ Packages/MyLayout/MySplit.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin
+import time
 
 class CloneFileAndSplit(sublime_plugin.WindowCommand):
   def run(self):
@@ -123,52 +124,3 @@ class MySplitTest(sublime_plugin.WindowCommand):
                 [0, 0, 2, 1], [2, 0, 4, 1],
                 [0, 1, 1, 2], [1, 1, 3, 2], [3, 1, 4, 2]]
     })
-
-class ReLayouter(sublime_plugin.EventListener):
-  def on_new(self, view):
-    # print "on_new: " + (view.name() or view.file_name() or "")
-    sublime.set_timeout(lambda: self.recheck_new(view), 100)
-
-  def recheck_new(self, view):
-    # print "recheck_new: " + (view.name() or view.file_name() or "")
-    if self.is_special(view):
-      if self.window and self.window.num_groups() > 1:
-        g, i = self.window.get_view_index(view)
-        if g != 1:
-          self.window.set_view_index(view, 1, len(self.window.views_in_group(1)))
-
-  def on_deactivated(self, view):
-    # print "on_deactivated: " + (view.name() or view.file_name() or "")
-    self.pv = view
-
-  def on_activated(self, view):
-    # print "on_activated: " + (view.name() or view.file_name() or "")
-    self.cv = view
-    self.window = view.window()
-    if self.needs_relayout():
-      print "needs relayout: " + (view.name() or view.file_name() or "")
-      self.relayout()
-
-  def is_special(self, view):
-    name = view.name() or ""
-    if name.startswith("myke ") or name == "Find Results":
-      return True
-
-  def needs_relayout(self):
-    if hasattr(self, "pv") and hasattr(self, "cv") and self.window and self.window.num_groups() > 1:
-      pg, pi = self.window.get_view_index(self.pv)
-      cg, ci = self.window.get_view_index(self.cv)
-      if pg == cg and self.pv != self.cv:
-        pn = self.pv.name() or ""
-        if pn.startswith("myke ") or pn == "Find Results":
-          return True
-
-  def relayout(self):
-    # crashes!! very weird
-    # pg, pi = self.window.get_view_index(self.pv)
-    # if pg != 1:
-    #   self.window.set_view_index(self.pv, 1, len(self.window.views_in_group(1)))
-    cg, ci = self.window.get_view_index(self.cv)
-    if cg != 0 and not self.is_special(self.cv):
-      self.window.set_view_index(self.cv, 0, len(self.window.views_in_group(0)))
-    self.window.focus_view(self.cv)
