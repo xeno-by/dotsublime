da736f2f6f1273355a2479fa77638a7daf5db334
Eugene Burmako <xeno.by@gmail.com>
2012-09-18 18:01:21 +0200

    2219

--- Packages/MyPlayground/playground.py
+++ Packages/MyPlayground/playground.py
@@ -4,14 +4,22 @@ from sublime_plugin import *
 lines = [[str(i), str(i+1)] for i in range(1, 100000)]
 # lines = [str(i) for i in range(1, 100000)]
 
-class MyPlaygroundCommand(WindowCommand):
-  def run(self):
-    sublime.set_timeout(self.foo, 3000)
-    self.window.show_quick_panel(lines, lambda _: None)
+# class MyPlaygroundCommand(WindowCommand):
+#   def run(self):
+#     sublime.set_timeout(self.foo, 3000)
+#     self.window.show_quick_panel(lines, lambda _: None)
 
-  def foo(self):
-    self.window.new_file()
-    self.window.show_quick_panel(lines, lambda _: None)
+#   def foo(self):
+#     self.window.new_file()
+#     self.window.show_quick_panel(lines, lambda _: None)
+
+class MyPlaygroundCommand(TextCommand):
+  def run(self, edit):
+    sel = self.view.sel()[0]
+    lines = self.view.substr(sel).split("\n")
+    for idx, line in enumerate(lines[:-1]):
+      lines[idx] = str(idx + 1) + ") " + lines[idx]
+    self.view.replace(edit, sel, "\n".join(lines))
 
 # class FuzzyEventListener(EventListener):
 #   def on_activated(self, view):
