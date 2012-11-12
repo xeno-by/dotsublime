41974300c5768af414a687abf1797249c0e130f7
Eugene Burmako <xeno.by@gmail.com>
2012-11-12 11:04:44 +0100

    wip

---
+++ Packages/MyHack/MyPartest.py
@@ -0,0 +1,25 @@
+import sublime, sublime_plugin
+from functools import partial as bind
+from subprocess import call, Popen, PIPE
+
+class MyPartestCreateCommand(sublime_plugin.WindowCommand):
+  def run(self, flags):
+    self.flags = flags
+    self.window.show_quick_panel(self.test_types(), bind(self.on_selected))
+
+  def test_types(self):
+    return ["run", "neg", "pos"]
+
+  def on_selected(self, index):
+    self.test_type = self.test_types()[index]
+    self.window.show_input_panel("Test name: ", "", self.on_entered, None, None)
+
+  def on_entered(self, name):
+    name = "files/" + self.test_type + "/" + name
+    script = Popen(["partest-create", name] + (["--create-flags"] if self.flags else []), stdout=PIPE)
+    output = script.communicate()[0][:-1]
+    call(["growlnotify", "-n", "Partest", "-m", output])
+    if script.returncode == 0:
+      created = map(lambda path: path[len("Created "):], output.splitlines())
+      files = filter(os.path.isfile, created)
+      for file in files: self.window.open_file(file)
