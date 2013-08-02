8e987d8a3d4973a1f5670ee3919f8c8da873c423
Eugene Burmako <xeno.by@gmail.com>
2013-08-02 20:43:49 +0200

    transforms my_hack into a helper for general-purpose subl-open-project

--- Packages/MyTweaks/MyOpenProject.py
+++ Packages/MyTweaks/MyOpenProject.py
@@ -1,16 +1,13 @@
 import sublime, sublime_plugin
-import os, re, shutil
-from functools import partial as bind
-from subprocess import Popen, PIPE
+import os
 
-class MyHackCommand(sublime_plugin.ApplicationCommand):
+class MyOpenProjectCommand(sublime_plugin.ApplicationCommand):
   def run(self):
-    command, project_file_name, action = open(os.path.expandvars("$HOME/.hack_sublime"), "r").read().splitlines()
-    add, delete = command.startswith("+"), command.startswith("-")
+    project_file_name, action = open(os.path.expandvars("$HOME/.sublime"), "r").read().splitlines()
     project_window = next(filter(lambda w: w.project_file_name() == project_file_name, sublime.windows()), None)
     if action == "OPEN?":
       status = "YEP" if project_window else "NOPE"
-      with open(os.path.expandvars("$HOME/.hack_sublime"), "w") as f: f.write(status)
+      with open(os.path.expandvars("$HOME/.sublime"), "w") as f: f.write(status)
     elif action == "FOCUS!":
       if project_window and sublime.active_window().id() != project_window.id():
         project_window.focus_group(project_window.active_group())
