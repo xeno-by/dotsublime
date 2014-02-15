2aa1b9a055466b95a1fb99099dd2c4aff8603159
Eugene Burmako <xeno.by@gmail.com>
2014-02-15 10:41:36 +0100

    WhiteboxContext => whitebox.Context

--- Packages/MyDev/Macro/211x/Macros.scala
+++ Packages/MyDev/Macro/211x/Macros.scala
@@ -1,8 +1,8 @@
-import scala.reflect.macros.WhiteboxContext
+import scala.reflect.macros.whitebox._
 import scala.language.experimental.macros
 
 object Macros {
-  def impl(c: WhiteboxContext) = {
+  def impl(c: Context) = {
     import c.universe._
     HERE
   }
