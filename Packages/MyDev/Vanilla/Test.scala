3c5ba9d9e1a5373619a2f27aca7443ba3c28900d
Eugene Burmako <xeno.by@gmail.com>
2015-01-23 14:21:21 +0100

    deprecates the repository in favor of https://github.com/xeno-by/dotx

--- Packages/MyDev/Vanilla/Test.scala
+++
@@ -1,8 +0,0 @@
-import scala.reflect.runtime.universe._
-import scala.reflect.runtime.{currentMirror => cm}
-import scala.tools.reflect.ToolBox
-import scala.tools.reflect.Eval
-
-object Test extends App {
-  HERE
-}
--- Packages/MyTweaks/MyClose.py
+++
@@ -1,8 +0,0 @@
-import sublime, sublime_plugin
-
-class MyCloseCommand(sublime_plugin.WindowCommand):
-  def run(self):
-    if self.window.views():
-      self.window.run_command("close")
-    else:
-      pass
